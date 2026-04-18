f75cfbd1b39a7e2574cad6ec86ecffdbba3447d3
Wikkenden <Wickendenwilliam@gmail.com>
2026-04-18 12:22:27 -0400

    Database table updates

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -9,10 +9,18 @@ export const users = sqliteTable("users", {
   passwordHash: text("password_hash").notNull(),
 })
 
-export const messages = sqliteTable("message", {
-   id: text("id").primaryKey(),
-   senderId: text("sender_id").notNull(),
-   receiverId: text("receiver_id").notNull(),
-   body: text("body").notNull(),
-   createdAt: integer("created_at", { mode: "timestamp_ms" }).notNull()
+export const messages = sqliteTable("messages", {
+  id: text("id").primaryKey(),
+  conversationId: text("conversation_id").notNull(),
+  senderId: text("sender_id").notNull(),
+  body: text("body").notNull(),
+  createdAt: integer("created_at", { mode: "timestamp_ms" }).notNull(),
+});
+
+export const conversations = sqliteTable("conversations", {
+  id: text("id").primaryKey(),
+  type: text("type").notNull(), // dm | group | channel
+  serverId: text("server_id"),
+  name: text("name"),
+  createdAt: integer("created_at", { mode: "timestamp_ms" }).notNull(),
 });
